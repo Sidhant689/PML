a9fd1935d641bb3d7f4384465cb3808aa3d870d4
Administrator <Administrator@SW045>
2025-05-26 14:55:21 +0530

    generic css

--- PMLERP/MauiProgram.cs
+++ PMLERP/MauiProgram.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using MudBlazor.Services;
 using PMLERP.Helpers;
 using PMLERP.IServices;
 using PMLERP.Services;
@@ -43,9 +42,6 @@ namespace PMLERP
 
             // Add authentication support
             builder.Services.AddAuthorizationCore();
-
-            // Add MudBlazor for UI components (optional but recommended)
-            builder.Services.AddMudServices();
 
             // Register secure storage
             //builder.Services.AddSingleton<ISecureStorage>(SecureStorage.Default);
